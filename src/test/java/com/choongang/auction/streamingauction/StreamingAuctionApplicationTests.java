a31f92084ce9df2f1615533f16ca2168a04e8ac7
inhyeokoh <oredtiger@naver.com>
2025-03-07 21:30:04 +0900

    프로젝트 초기 설정

---
+++ src/test/java/com/choongang/auction/streamingauction/StreamingAuctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choongang.auction.streamingauction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingAuctionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
