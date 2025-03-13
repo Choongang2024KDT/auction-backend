c9da3d104a6e217b8ad65631af9291d19b834e12
inhyeokoh <oredtiger@naver.com>
2025-03-13 18:09:30 +0900

    알림 기능 테스트 버전 추가

---
+++ src/main/java/com/choongang/auction/streamingauction/controller/NotificationController.java
@@ -0,0 +1,39 @@
+package com.choongang.auction.streamingauction.controller;
+
+import com.choongang.auction.streamingauction.domain.notification.dto.response.NotificationDto;
+import com.choongang.auction.streamingauction.domain.notification.entity.Notification;
+import com.choongang.auction.streamingauction.service.NotificationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+
+@RestController
+@RequestMapping("api/notifications")
+@RequiredArgsConstructor
+public class NotificationController {
+    private final NotificationService notificationService;
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<List<Notification>> findAll(@PathVariable Long userId) {
+        return ResponseEntity.ok(notificationService.findAll(userId));
+    }
+
+    @PostMapping("/send/{userId}")
+    public ResponseEntity<?> sendNotification(@PathVariable Long userId, @RequestBody NotificationDto dto) {
+        notificationService.sendNotification(userId, dto);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("/read/{id}")
+    public ResponseEntity<?> markAsRead(@PathVariable Long id) {
+        notificationService.markAsRead(id);
+
+        return ResponseEntity.ok().body(Map.of(
+                "message", "읽음 처리 되었습니다. id -" +id
+        ));
+    }
+}
---
+++ src/main/java/com/choongang/auction/streamingauction/service/SseEmitterService.java
@@ -0,0 +1,40 @@
+package com.choongang.auction.streamingauction.service;
+
+import com.choongang.auction.streamingauction.domain.notification.dto.response.NotificationDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+@Slf4j
+public class SseEmitterService {
+    private final Map<Long, SseEmitter> emitters = new ConcurrentHashMap<>();
+
+    // SSE 구독
+    public SseEmitter subscribe(Long userId) {
+        SseEmitter emitter = new SseEmitter(Long.MAX_VALUE);
+        emitters.put(userId, emitter);
+
+        emitter.onCompletion(() -> emitters.remove(userId));
+        emitter.onTimeout(() -> emitters.remove(userId));
+
+        log.info("SSE 구독 시작");
+
+        return emitter;
+    }
+
+    public void sendNotification(Long userId, NotificationDto notification) {
+        SseEmitter emitter = emitters.get(userId);
+        if (emitter != null) {
+            try {
+                emitter.send(SseEmitter.event().data(notification));
+            } catch (IOException e) {
+                emitters.remove(userId);
+            }
+        }
+    }
+}
