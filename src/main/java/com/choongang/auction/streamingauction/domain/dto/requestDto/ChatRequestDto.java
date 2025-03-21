6e72087f41d0f52859f0b7cc9bce801ecafbe038
tony24123 <tjdduswns01@naver.com>
2025-03-21 11:09:04 +0900

    ChatRequestDto 입력시간 , 유저 닉네임 추가

--- src/main/java/com/choongang/auction/streamingauction/domain/dto/requestDto/ChatRequestDto.java
+++ src/main/java/com/choongang/auction/streamingauction/domain/dto/requestDto/ChatRequestDto.java
@@ -2,7 +2,9 @@ package com.choongang.auction.streamingauction.domain.dto.requestDto;
 
 public record ChatRequestDto(
         Long memberId,
+        String nickName,
         Long auctionId,
-        String message
+        String message,
+        String timestamp // 타임스탬프 추가
 ) {
 }
