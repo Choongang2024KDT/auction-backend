51d0cb15d224246203ce5b0277eb382a1fef90c8
tony24123 <tjdduswns01@naver.com>
2025-03-31 11:33:24 +0900

    Revert "emitter 타임아웃 10분 -> 1시간"
    This reverts commit 0e3df2e563cfbbf9f95a3b7d50604035c17c41f0.

--- src/main/java/com/choongang/auction/streamingauction/service/SseEmitterService.java
+++ src/main/java/com/choongang/auction/streamingauction/service/SseEmitterService.java
@@ -42,7 +42,7 @@ public class SseEmitterService {
         }
 
         // 새 emitter
-        SseEmitter emitter = new SseEmitter(3_600_000L); // 타임아웃 1시간
+        SseEmitter emitter = new SseEmitter(600_000L); // 타임아웃 10분
         emitters.put(key, emitter);
         log.info("Created emitter for key: {}, Current emitters map: {}", key, emitters.keySet());
 
